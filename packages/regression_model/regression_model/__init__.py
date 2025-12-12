8f5809591ea204c622e3649c8cf6ae2cc9e07f8e
Soulbah313 <63166287+Soulbah313@users.noreply.github.com>
2025-12-12 15:04:17 -0500

    Partie-7 Création du package building

--- packages/regression_model/regression_model/__init__.py
+++ packages/regression_model/regression_model/__init__.py
@@ -7,7 +7,7 @@ from regression_model.config import logging_config
 VERSION_PATH = config.PACKAGE_ROOT / 'VERSION'
 
 # Configure logger for use in package
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("regression_model")
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging_config.get_console_handler())
 logger.propagate = False
